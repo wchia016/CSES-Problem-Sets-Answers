2f9c0d8f5f6b3bf53a1437a9e29c481d5b8414d5
Chia Wei Fong <65015142+wchia016@users.noreply.github.com>
2020-06-20 19:37:11 +0800

    Add files via upload

---
+++ Introductory Problems/Increasing Array.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 19:16:25 2020
+
+@author: Wei Fong
+"""
+
+
+#arr_size = 10
+#arr_contents = [1000000000, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+arr_size = int(input())
+arr_contents = list(map(int, input().split()))
+
+turns = 0
+for i in range(1,arr_size):
+    if arr_contents[i] < arr_contents[i-1]:
+        diff = (arr_contents[i-1] - arr_contents[i])
+        turns += diff
+        arr_contents[i] += diff
+        
+print(turns)
+
